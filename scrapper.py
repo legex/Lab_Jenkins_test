576da0766f1f2eb05b0488e2d76010e9fecf0281
legex <abhishekshukla.sh@gmail.com>
2023-05-21 13:38:53 +0000

    updated scrapper

--- scrapper.py
+++ scrapper.py
@@ -4,17 +4,17 @@ from datetime import*
 import re
 import sslbypass
 
-linkcontent=[]
-
 class URLAccess():
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def urlaccessed(url):
-        req=sslbypass.get_legacy_session().get(url)
-        ur=req.text
-        soup=BeautifulSoup(ur,'html.parser')
-        for link in soup.find_all('a', attrs={'href': re.compile('^/c/dam/en/us/td/docs/')}):
-            activelinks= 'https://www.cisco.com'+(link.get('href'))
-            linkcontent.append(activelinks)
-        return linkcontent
+        try:
+            req=sslbypass.get_legacy_session().get(url)
+            return req.status_code
+        except ConnectionError:
+            return "404 Not found"
+
+
+if __name__== '__main__':
+    run=URLAccess()
